96bd2616407309675f82e6dbaf79fedf60e53868
dongsu.han <105100402+handongsu@users.noreply.github.com>
2023-07-17 14:28:40 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,6 +1,13 @@
 function solution(seoul) {
     var answer = '';
-    var find = seoul.indexOf('Kim');
-    answer = `김서방은 ${find}에 있다`
-    return answer;
+    let x = 0;
+    for(let i = 0; i < seoul.length; i++){
+        if(seoul[i] == "Kim"){
+            x = i
+            break;
+        }
+    }
+    // var find = seoul.indexOf('Kim');
+    // answer = `김서방은 ${find}에 있다`
+    return "김서방은 " + x + "에 있다";
 }
