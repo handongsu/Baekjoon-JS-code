57383a3f9d764cdb7062631f6bbd8cb812779575
dongsu.han <105100402+handongsu@users.noreply.github.com>
2023-07-18 15:30:50 +0900

    [level 1] Title: 3진법 뒤집기, Time: 2.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    var answer = 0;
+    answer = n.toString(3).split('').reverse().join('');
+    answer= parseInt(answer, 3);
+    
+    console.log(answer)
+    return answer;
+}
