de2e22128d68c0eaae8f9fc5c6bd722b0acf3468
dongsu.han <105100402+handongsu@users.noreply.github.com>
2023-08-01 21:53:31 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 3.64 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,18 +1,21 @@
 function solution(s) {
-  var answer = "";
-  let arr = s.split(" ");
-
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      if (j % 2 == 0) {
-        answer += arr[i][j].toUpperCase();
-      } else {
-        answer += arr[i][j].toLowerCase();
-      }
+    var answer = '';
+    var arr = [];
+    let str = s.split(' '); // ..?
+    console.log(str);
+    for(let i = 0; i < str.length; i++){    // 3번
+        for(let j = 0; j < str[i].length; j++){ //문자갯수
+        if(j % 2 === 0){
+            arr.push(str[i][j].toUpperCase());
+        }else{
+            arr.push(str[i][j].toLowerCase());
+        }
     }
-    if (i < arr.length - 1) {
-      answer += " ";
+         if(i < (str.length - 1)){
+             arr.push(' ');
+         }
     }
-  }
-  return answer;
+    answer = arr.join('');
+    
+    return answer;
 }
