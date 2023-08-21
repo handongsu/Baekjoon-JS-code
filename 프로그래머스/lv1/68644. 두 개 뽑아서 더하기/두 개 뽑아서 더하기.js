6acd979e967691fda0bf7d794e2c1fa4e27ce63b
dongsu.han <105100402+handongsu@users.noreply.github.com>
2023-08-21 23:47:51 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.67 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -3,16 +3,16 @@ function solution(numbers) {
     let arr = [];
     for(let i = 0; i < numbers.length; i++){
         for(let j = i+1; j < numbers.length; j++){
-            answer.push(numbers[i] + numbers[j]); 
+            arr.push(numbers[i] + numbers[j]); 
         }
     }
-    answer.forEach(e => {
-    if (!arr.includes(e)) {
-        arr.push(e);
+    arr.forEach(e => {
+    if (!answer.includes(e)) {
+        answer.push(e);
     }
 });
    
-    return arr.sort((a,b) => a - b);
+    return answer.sort((a,b) => a - b);
 }
 
 //2중for 안에 조건문
