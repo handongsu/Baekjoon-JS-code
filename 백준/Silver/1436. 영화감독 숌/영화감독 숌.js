f8a1abb0242ee16385ae83fd338d2df3fcc51058
dongsu.han <105100402+handongsu@users.noreply.github.com>
2023-12-03 21:52:04 +0900

    [Silver V] Title: 영화감독 숌, Time: 440 ms, Memory: 23240 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
@@ -0,0 +1,14 @@
+const fs=require('fs');
+var input=fs.readFileSync('/dev/stdin','utf-8').toString();
+
+let title=666;
+
+while(true){   
+    if(title.toString().includes('666')){
+        input-=1;
+        if(input==0)
+            break;
+    }   
+    title+=1;   
+}
+console.log(title);
